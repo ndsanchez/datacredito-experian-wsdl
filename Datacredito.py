bf93e042959d90ce8cbb67a9252fac5e1d3fff36
Neil Sanchez <neil.sanchez@bodytechcorp.com>
2023-06-16 20:53:34 +0000

    wsdl working with class

--- Datacredito.py
+++ Datacredito.py
@@ -13,32 +13,32 @@ class Datacredito():
     def __init__(self):
         load_dotenv()
 
-        self.wsdl_url = os.getenv('DATACREDITO_WSDL_URL')
-        self.cerfile_path = os.getenv('CERTFILE_PATH')
-        self.private_key_path = os.getenv('PRIVATE_KEY_PATH')
-        self.okta_username = os.getenv('OKTA_USERNAME')
-        self.okta_password = os.getenv('OKTA_PASSWORD')
+        wsdl_url = os.getenv('DATACREDITO_WSDL_URL')
+        cerfile_path = os.getenv('CERTFILE_PATH')
+        private_key_path = os.getenv('PRIVATE_KEY_PATH')
+        okta_username = os.getenv('OKTA_USERNAME')
+        okta_password = os.getenv('OKTA_PASSWORD')
         self.short_key = os.getenv('DATACREDITO_SHORT_KEY')
         self.user = os.getenv('DATACREDITO_USER')
 
-        self.session = Session()
-        self.session.cert = (cerfile_path, private_key_path)
-        self.transport = Transport(session=session)
+        session = Session()
+        session.cert = (cerfile_path, private_key_path)
+        transport = Transport(session=session)
 
-        self.wsse = Signature(private_key_path, cerfile_path)
-        self.username_token = UsernameToken(okta_username, okta_password)
+        wsse = Signature(private_key_path, cerfile_path)
+        username_token = UsernameToken(okta_username, okta_password)
 
         self.client = Client(wsdl_url, wsse=CustomSignature([username_token, wsse]), transport=transport)
 
-    def getAccounts(self, doc_type, doc_number):
+    def getAccounts(self, doc_type, doc_number, last_name):
         # metodos = dir(client.service)
         payload = {
             'clave': self.short_key,
             # 'identificacion':'70139881',
             # 'primerApellido':'MALDONADO',
-            'identificacion':'1189213694',# 70139881, #1189213694
-            'primerApellido':'SANCHEZ',
-            'tipoIdentificacion':'1',
+            'identificacion': doc_number,# 70139881, #1189213694
+            'primerApellido': last_name,
+            'tipoIdentificacion': doc_type,
             'producto':'64',
             'usuario': self.user,
         }
---
+++ index.py
@@ -0,0 +1,5 @@
+from Datacredito import Datacredito
+
+datacredito = Datacredito()
+
+datacredito.getAccounts('1', '1189213694', 'SANCHEZ')
